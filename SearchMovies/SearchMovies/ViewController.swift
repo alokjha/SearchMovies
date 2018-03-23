77d2a16d686fbc70fbbd511b3681490057bed4a9
Alok Jha <alok4588@gmail.com>
2018-03-23 15:41:05 +0530

    initial commit of xcodeproject

---
+++ SearchMovies/SearchMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchMovies
+//
+//  Created by Alok Jha on 23/03/18.
+//  Copyright © 2018 Alok Jha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
